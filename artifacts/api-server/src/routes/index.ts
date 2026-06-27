c62388694119c3c62e54d520ef617a8a9387e3fb
Yasin Miyade <yasinmiyade66@gmail.com>
2026-06-27 15:09:34 +0530

    fix: remove schemaLocation attributes from sitemap root urlset tag to prevent XML validation schema errors

--- artifacts/api-server/src/routes/index.ts
+++ artifacts/api-server/src/routes/index.ts
@@ -174,10 +174,7 @@ router.get("/sitemap.xml", async (req, res) => {
 
     const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <?xml-stylesheet type="text/xsl" href="/sitemap.xsl"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-        xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-          http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${allPages.map((p) => `  <url>
     <loc>${host}${p.url}</loc>
     <lastmod>${now}</lastmod>
